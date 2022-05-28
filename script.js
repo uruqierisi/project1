6a10b1769f5234ed17b6c1e6fc7259d39a1a69d6
kodeksi917 <101365096+kodeksi917@users.noreply.github.com>
2022-05-29 00:10:55 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function toggle() {
+  let sec = document.getElementById("sec");
+  let nav = document.getElementById("navigation");
+  sec.classList.toggle("active");
+  nav.classList.toggle("active");
+}
